128bde7485de1985cdf13bd496842a47e93efc3c
Pixer_Scrounger <elitistthief@gmail.com>
2019-01-06 16:42:16 +0200

    fixing build script

--- build.ps1
+++ build.ps1
@@ -1,16 +1,16 @@
 #Requires -Version 5.0
 
-$NugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-$ToolsDir = Join-Path $PSScriptRoot "BuildTools
-$NugetExe = Join-Path $ToolsDir "nuget.exe"
-$Solution = "src/PhpTravels.UITests.sln"
+$NugetUrl = 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe'
+$ToolsDir = Join-Path $PSScriptRoot 'BuildTools'
+$NugetExe = Join-Path $ToolsDir 'nuget.exe'
+$Solution = 'src/PhpTravels.UITests.sln'
 
-Write-Verbose "Script Root is: $PSScriptRoot"
+Write-Verbose 'Script Root is: $PSScriptRoot'
 
-Write-Verbose "Checking for NuGet in tools path..."
+Write-Verbose 'Checking for NuGet in tools path...'
 if (-Not (Test-Path $NugetExe -Verbose:$VerbosePreference)) 
 {
-    Write-Output "Installing NuGet from $NugetUrl..."
+    Write-Output 'Installing NuGet from $NugetUrl...'
     Invoke-WebRequest $NugetUrl -OutFile $NugetExe -Verbose:$VerbosePreference -ErrorAction Stop
 }
 
